60638b99ebab713e169e25d148ea15130dec4abd
Amgad Malik <amgadmalik@yahoo.com>
2019-10-20 19:00:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
   end 
   
   def valid 
-    if side_1 > 0 || side_2 > 0 || side_3 > 0
+    if side_1 > 0 && side_2 > 0 && side_3 > 0
       if 
         (side_1 + side_2) > side_3
         if (side_1 + side_3) > side_2
